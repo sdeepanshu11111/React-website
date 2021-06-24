a65661977e331a2556948256efebddfa953e5e59
sdeepanshu11111 <sdeepanshu1111@gmail.com>
2021-06-24 14:17:21 +0530

    2. About section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+// import pages from
+import AboutPage from "./pages/aboutUs";
+
 function App() {
   return (
     <div className="App">
       <h1>This is capture</h1>
+      <AboutPage />
     </div>
   );
 }
---
+++ src/pages/aboutUs.js
@@ -0,0 +1,13 @@
+// import components
+
+import AboutSection from "../components/AboutSection";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <AboutSection />
+    </div>
+  );
+};
+
+export default AboutPage;
