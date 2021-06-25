e806dfb943e87b80b7bcaef8e1d9ebb5c8f11cd5
sdeepanshu11111 <sdeepanshu1111@gmail.com>
2021-06-25 05:33:06 +0530

    faq

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -13,6 +13,16 @@ const FaqSection = () => {
         </div>
       </div>
       <div className="question">
+        <h4>What products do you offer</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt,
+            eaque?
+          </p>
+        </div>
+      </div>
+      <div className="question">
         <h4>Daily Schedule</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
@@ -23,17 +33,7 @@ const FaqSection = () => {
         </div>
       </div>
       <div className="question">
-        <h4>How do i start</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt,
-            eaque?
-          </p>
-        </div>
-      </div>
-      <div className="question">
-        <h4>How do i start</h4>
+        <h4>Different payments methods</h4>
         <div className="answer">
           <p>Lorem ipsum dolor sit amet.</p>
           <p>
--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -1,14 +1,15 @@
 // import components
 
 import AboutSection from "../components/AboutSection";
-
 import ServicesSection from "../components/ServicesSection";
+import Faq from "../components/FaqSection";
 
 const AboutPage = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <Faq />
     </>
   );
 };
